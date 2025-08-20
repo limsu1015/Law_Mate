f328af8e47fc580ae7dbc73d94c54212451c49a6
limsu1015 <tngur1015@gmail.com>
2025-08-20 14:11:08 +0900

    프로젝트 기본 설정

---
+++ src/config/__init__.py
@@ -0,0 +1,22 @@
+import os
+from enum import StrEnum
+
+from pydantic.v1 import BaseSettings
+
+
+class ServerEnv(StrEnum):
+    LOCAL = "local"
+    DEV = "dev"
+    PROD = "prod"
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str
+    PROJECT_DESCRIPTION: str
+    ASYNC_DATABASE_URL: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
